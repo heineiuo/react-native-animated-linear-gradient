93eee844906e23b47666a2dc89e0a7302d91564a
heineiuo <heineiuo@gmail.com>
2016-10-23 22:19:08 +0800

    releast 1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # react-native-animated-linear-gradient
 Animated linear gradient as background animation or other.
+
+
+## Install
+
+1. install [react-native-linear-gradient](https://github.com/react-native-community/react-native-linear-gradient),  run `npm install react-native-linear-gradient --save` 
+2. run `react-native link react-native-linear-gradient`
+3. run `npm install react-native-animated-linear-gradient --save`
+
+
+## Use
+
+```
+import React, { Component } from 'react';
+import { AppRegistry, Text } from 'react-native';
+import AnimatedLinearGradient from 'react-native-animated-linear-gradient'
+
+class HelloWorldApp extends Component {
+  render() {
+    return (
+      <AnimatedLinearGradient />
+    );
+  }
+}
+
+AppRegistry.registerComponent('HelloWorldApp', () => HelloWorldApp);
+```
+
+## Props
+
+1. colors
+
+This module incluse some preset colors:
+
+```
+export presetColors = {
+  instagram: [
+    'rgb(106, 57, 171)',
+    'rgb(151, 52, 160)',
+    'rgb(197, 57, 92)',
+    'rgb(231, 166, 73)',
+    'rgb(181, 70, 92)'
+  ],
+  firefox: [
+    'rgb(236, 190, 55)',
+    'rgb(215, 110, 51)',
+    'rgb(181, 63, 49)',
+    'rgb(192, 71, 45)',
+  ],
+  sunrise: [
+    'rgb(92, 160, 186)',
+    'rgb(106, 166, 186)',
+    'rgb(142, 191, 186)',
+    'rgb(172, 211, 186)',
+    'rgb(239, 235, 186)',
+    'rgb(212, 222, 206)',
+    'rgb(187, 216, 200)',
+    'rgb(152, 197, 190)',
+    'rgb(100, 173, 186)',
+  ]
+};
+
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,132 @@
+import React, {Component, PropTypes} from 'react';
+import {StyleSheet, StatusBar, Dimensions, View, Animated, Easing} from 'react-native';
+import NativeLinearGradient from 'react-native-linear-gradient';
+import rgb2hex from 'rgb2hex';
+
+// const {height, width} = Dimensions.get('window');
+
+class LinearGradient extends Component {
+  render () {
+    const {color0, color1} = this.props;
+    return (
+      <NativeLinearGradient
+        // colors={this.props.colors.map((c) => rgb2hex(c).hex)}
+        colors={[color0, color1].map((c) => rgb2hex(c).hex)}
+        start={[0, 0.4]}
+        end={[1, 0.6]}
+        style={[styles.linearGradient]} />
+    )
+  }
+}
+
+Animated.LinearGradient = Animated.createAnimatedComponent(LinearGradient)
+// Animated.NativeLinearGradient = Animated.createAnimatedComponent(NativeLinearGradient)
+
+export const presetColors = {
+  instagram: [
+    'rgb(106, 57, 171)',
+    'rgb(151, 52, 160)',
+    'rgb(197, 57, 92)',
+    'rgb(231, 166, 73)',
+    'rgb(181, 70, 92)'
+  ],
+  firefox: [
+    'rgb(236, 190, 55)',
+    'rgb(215, 110, 51)',
+    'rgb(181, 63, 49)',
+    'rgb(192, 71, 45)',
+  ],
+  sunrise: [
+    'rgb(92, 160, 186)',
+    'rgb(106, 166, 186)',
+    'rgb(142, 191, 186)',
+    'rgb(172, 211, 186)',
+    'rgb(239, 235, 186)',
+    'rgb(212, 222, 206)',
+    'rgb(187, 216, 200)',
+    'rgb(152, 197, 190)',
+    'rgb(100, 173, 186)',
+  ]
+};
+
+class AnimatedGradient extends Component {
+
+  static defaultProps = {
+    customColors: presetColors.instagram,
+    speed: 4000
+  }
+
+  state = {
+    color0: new Animated.Value(0),
+    color1: new Animated.Value(0),
+  }
+
+  componentDidMount = () => {
+    this.startAnimation();
+  }
+
+  startAnimation = () => {
+    const {color0, color1} = this.state;
+    const {customColors, speed} = this.props;
+    [color0, color1].forEach(color => color.setValue(0));
+
+    Animated.parallel(
+      [color0, color1].map(animatedColor => {
+        return Animated.timing(animatedColor, {
+          toValue: customColors.length,
+          duration: customColors.length * speed,
+          easing: Easing.linear
+        })
+      })
+    )
+      .start(this.startAnimation);
+
+  };
+
+  render () {
+
+    const {color0, color1} = this.state;
+    const {customColors} = this.props;
+    const preferColors = [];
+    // while (preferColors.length < customColors.length) {
+    while (preferColors.length < 2) {
+      preferColors.push(
+        customColors
+          .slice(preferColors.length)
+          .concat(customColors.slice(0, preferColors.length+1))
+      )
+    }
+    const interpolatedColors = [color0, color1]
+      .map((animatedColor, index) => {
+        return animatedColor.interpolate({
+          inputRange: Array.from({length: customColors.length+1}, (v, k) => k),
+          outputRange: preferColors[index]
+        })
+      });
+
+    return (
+      <Animated.LinearGradient
+        style={[styles.linearGradient]}
+        // colors={interpolatedColors}
+        color0={interpolatedColors[0]}
+        color1={interpolatedColors[1]} />
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    position: 'absolute',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0
+  }
+});
+
+
+export default AnimatedGradient
