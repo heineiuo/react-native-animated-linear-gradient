2792fd6f86bfe7eeaccd78f849dc80dd1cc401ff
heineiuo <heineiuo@gmail.com>
2017-05-04 00:46:42 +0800

    add an example

---
+++ examples/simple/index.ios.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  Navigator,
+  View
+} from 'react-native';
+import AnimatedLinearGradient, {presetColors} from 'react-native-animated-linear-gradient'
+
+
+export default class simple extends Component {
+  render() {
+    return (
+      <AnimatedLinearGradient customColors={presetColors.firefox} speed={4000}/>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+AppRegistry.registerComponent('simple', () => simple);
