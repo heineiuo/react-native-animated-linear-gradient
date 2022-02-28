f7bea5b2ede876799ec5f45653fd7bfc2cfe8060
Oscar Enrique Garcia Lugo <36555905+os2mem@users.noreply.github.com>
2022-02-28 04:25:11 +0100

    Fix warning of useNativeDriver (#61)
    * Fix warning of useNativeDriver
    
    * Remove line. Change default value.

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ class LinearGradient extends Component {
   }
 }
 Animated.LinearGradient = Animated.createAnimatedComponent(LinearGradient)
-Animated.useNativeDriver = true
 // Animated.NativeLinearGradient = Animated.createAnimatedComponent(NativeLinearGradient)
 
 export const presetColors = {
@@ -84,7 +83,8 @@ class AnimatedGradient extends Component {
         return Animated.timing(animatedColor, {
           toValue: customColors.length,
           duration: customColors.length * speed,
-          easing: Easing.linear
+          easing: Easing.linear,
+          useNativeDriver: this.props.useNativeDriver || false,
         })
       })
     )
